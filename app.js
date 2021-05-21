04dbd7204bab4b0c3d32958aaa82335248882b87
myrmnn <myrm.nn@gmail.com>
2021-05-21 09:06:23 -0400

    centered form

--- app.js
+++ app.js
@@ -36,13 +36,3 @@ const fields = ["firstname", "lastname", "email", "password"];
 const validator = new FormValidator(form, fields);
 
 validator.initialize();
-
-// const email = document.querySelector(".email");
-
-// email.addEventListener("input", function (event) {
-// 	if (email.validity.typeMismatch) {
-// 		email.setCustomValidity("I am expecting an e-mail address!");
-// 	} else {
-// 		email.setCustomValidity("");
-// 	}
-// });
